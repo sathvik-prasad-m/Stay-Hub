c26adc18497036b2734cf8bd89e00bce783fc988
Sathvik Prasad M <2835sathvikprasad.m@gmail.com>
2025-05-02 01:28:30 +0530

    Resolve merge conflict in index.js

---
+++ init/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken =
+  "pk.eyJ1Ijoic2F0aHZpazI4IiwiYSI6ImNtYTB6Y2RqejFrcnYyanNnOWwwbTVmNGsifQ.Hqk_IUQpBTwNo7pjQu38-A";
+const geocdingClient = mbxGeocoding({ accessToken: mapToken });
+
+main()
+  .then(() => console.log("DB connected Successfully"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb+srv://Sathvik:SuVfTERqkepI0vdH@cluster0.dg7wi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+}
+
+let init = async () => {
+  await Listing.deleteMany({});
+  for (listing of initData.data) {
+    let response = await geocdingClient
+      .forwardGeocode({
+        query: listing.location,
+        limit: 1,
+      })
+      .send();
+    listing.owner = "6813803899ec8e1f8916188d";
+    listing.geometry=response.body.features[0].geometry;
+  }
+  await Listing.insertMany(initData.data);
+  console.log("Data Initialized...");
+
+  mongoose.connection.close();
+};
+
+init();
